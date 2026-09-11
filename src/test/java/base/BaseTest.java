25344a5331341805ca593714c9fc83bb0e5e4cca
Angelica  Julieth Silva  Perez <angelica.silva01@globant.com>
2026-09-10 22:27:08 -0500

    Add BaseTest class

---
+++ src/test/java/base/BaseTest.java
@@ -0,0 +1,21 @@
+package base;
+
+import drivers.DriverManager;
+import io.appium.java_client.android.AndroidDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+
+    protected AndroidDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        driver = DriverManager.getDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        DriverManager.quitDriver();
+    }
+}
